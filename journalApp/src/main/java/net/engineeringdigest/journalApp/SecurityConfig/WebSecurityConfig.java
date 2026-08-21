f8cc7ec4c21113a4db50204d446e8485dc131a6a
Afaq alam <afaqlam323@gmail.com>
2026-08-21 17:58:59 +0530

    changed endpoint auth ways in webConfig

--- journalApp/src/main/java/net/engineeringdigest/journalApp/SecurityConfig/WebSecurityConfig.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/SecurityConfig/WebSecurityConfig.java
@@ -47,7 +47,7 @@ public class WebSecurityConfig{
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests(request -> request
-                        .antMatchers("/login", "/signup", "/greetings/*").permitAll()
+                        .antMatchers("/api/login", "/api/signup", "/api/greetings/*").permitAll()
                         .antMatchers("/admin/**", "/audio/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
                 .csrf(AbstractHttpConfigurer::disable)
